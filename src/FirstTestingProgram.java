9f75d3efb81d3528d756a41644fc1e9d71fe0d47
Julia Gatina <y.zinatullina@mail.ru>
2025-08-01 17:12:03 +0500

    Check division by 0

--- src/FirstTestingProgram.java
+++ src/FirstTestingProgram.java
@@ -17,7 +17,14 @@ public class FirstTestingProgram {
 
         System.out.println("a + b: " + (a + b));
         System.out.println("a - b: " + (a - b));
-        System.out.println("a / b: " + (a / b));
+
+
+        if (b != 0) {
+            System.out.println("a / b: " + (a / b));
+        } else {
+            System.out.println("a / b: Деление на ноль невозможно!");
+        }
+
         System.out.println("a x b: " + (a * b));
     }
 }
